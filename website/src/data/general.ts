e58fa5fbf0bb9ce437b7507a3f41ce6b2f6667f1
ricardoSA54 <ricardosa54@gmail.com>
2025-06-26 11:54:32 +0200

    load logo and signature from conf file instead of hardcoding

--- website/src/data/general.ts
+++ website/src/data/general.ts
@@ -5,12 +5,13 @@ export interface GeneralParameters {
     surname: string;
     portfolioTitle: string;
     logo: string;
+    signature: string;
     email: string;
     phone: string;
     address: string;
   }
 
 export const generalParameters: GeneralParameters =
-    { name: "John", surname: "Doe", portfolioTitle: "My Portfolio", logo:"../assets/generic_portrait.svg" ,
+    { name: "John", surname: "Doe", portfolioTitle: "My Portfolio", logo:"/src/assets/generic_portrait.svg" , signature: "/src/assets/generic_signature.png",
         email: "johndoe@email.com", phone: "9999999999", address: "rue de Cockayne, 1234"
     };
