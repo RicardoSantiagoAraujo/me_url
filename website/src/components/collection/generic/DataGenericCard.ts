a19f7ddc7cf580bd0575f25466e47a44f67333bc
ricardoSA54 <ricardosa54@gmail.com>
2025-07-02 21:10:24 +0200

    improve card system

---
+++ website/src/components/collection/generic/DataGenericCard.ts
@@ -0,0 +1,6 @@
+export interface DataGenericCard {
+    header: {right : string, left: string},
+    body: {text: string, img: string},
+    footer:  {right : string, left: string},
+    url: string
+ }
