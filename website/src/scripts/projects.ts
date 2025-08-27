2eac950c9d60f3db80a9bd1f513bcc4f5456930b
Ricardo Santiago Araujo <ricardo.santiago-araujo@livingpackets.com>
2025-08-27 13:38:55 +0200

    allow webp

--- website/src/scripts/projects.ts
+++ website/src/scripts/projects.ts
@@ -1,7 +1,7 @@
 
 export  function getImageFromProject(whichImgId:"idMainImg" | "idBgImg",data: any){
     const meta = data.images.find((img: any) => img.id === data[whichImgId]) || null;
-    const images = import.meta.glob<{ default: ImageMetadata }>('/src/assets/**/*.{jpeg,jpg,png,gif,svg}')
+    const images = import.meta.glob<{ default: ImageMetadata }>('/src/assets/**/*.{jpeg,jpg,webp,png,gif,svg}')
     const img = meta ? images[meta.url] ? images[meta.url]() : meta.url : null;
     return {img, meta}
 }
