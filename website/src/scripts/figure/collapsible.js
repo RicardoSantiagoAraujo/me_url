302ee4ea4231bb3cbe2587ec2e8bd52d06a720ab
ricardoSA54 <ricardosa54@gmail.com>
2025-09-26 23:40:16 +0200

    Continue import of Figure component

---
+++ website/src/scripts/figure/collapsible.js
@@ -0,0 +1,23 @@
+export function Collapsible() {
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+
+  // if the value is an empty string, remove collapsible
+  if (coll[i].nextElementSibling.innerHTML.replace(/\s+/g,'') == ""){
+    coll[i].parentElement.style.display="none";
+  }
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+}
---
+++ website/src/scripts/figure/main.ts
@@ -0,0 +1,7 @@
+import {Projects} from "./projects.js"
+import { Collapsible } from "./collapsible.js"
+import { Fullscreen_slideshow } from "./fullscreen_slideshow.js"
+
+Projects();
+Fullscreen_slideshow();
+Collapsible();
