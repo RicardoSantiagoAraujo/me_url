d85df57ad0541c19b1f17542c4b820863efa13e0
ricardoSA54 <ricardosa54@gmail.com>
2025-11-01 23:48:00 +0100

    improve techstack functionality

--- website/src/data/_general.ts
+++ website/src/data/_general.ts
@@ -1,7 +1,7 @@
 // This file sets the general parameters for the application
 import type { GeneralParameters } from "./types/GeneralParameters";
 import { generalParametersUser } from "./general.config";
-
+import { techstackDefault } from "./_techstack";
 // DEFINE USER PARAMETERS IN general.config.ts IN SAME DIRECTORY AS THIS FILE
 
 // PLACEHOLDER GENERAL PARAMETERS
@@ -66,128 +66,7 @@ const generalParametersPlaceholder: GeneralParameters = {
       hyperref: "instagram.com",
     },
   ],
-  techstack: {
-    astro: {
-      name: "Astro",
-      icon: "tech/astro",
-      link: "https://astro.build/",
-    },
-    javascript: {
-      name: "JavaScript",
-      icon: "tech/typescript",
-      link: "https://www.typescriptlang.org/",
-    },
-    typescript: {
-      name: "TypeScript",
-      icon: "tech/typescript",
-      link: "https://www.typescriptlang.org/",
-    },
-    jquery: {
-      name: "jQuery",
-      icon: "tech/jquery",
-      link: "https://jquery.com/",
-    },
-    bootstrap: {
-      name: "Bootstrap",
-      icon: "tech/bootstrap",
-      link: "https://getbootstrap.com/",
-    },
-    python: {
-      name: "Python",
-      icon: "tech/python",
-      link: "https://www.python.org/",
-    },
-    flask: {
-      name: "Flask",
-      icon: "tech/flask",
-      link: "https://flask.palletsprojects.com/en/stable/",
-    },
-    pyspark: {
-      name: "PySpark",
-      icon: "tech/pyspark2",
-      link: "https://spark.apache.org/docs/latest/api/python/",
-    },
-    psytoolkit: {
-      name: "PsyToolkit",
-      icon: "tech/r_colorless",
-      link: "https://www.psytoolkit.org/experiment-library/",
-    },
-    jsx: {
-      name: "JSX",
-      icon: "tech/jsx",
-      link: "https://fr.legacy.reactjs.org/docs/introducing-jsx.html",
-    },
-    html: {
-      name: "HTML",
-      icon: "tech/html",
-      link: "https://developer.mozilla.org/fr/docs/Web/HTML",
-    },
-    scss: {
-      name: "SCSS",
-      icon: "tech/sass",
-      link: "https://sass-lang.com/",
-    },
-    mysql: {
-      name: "SQL",
-      icon: "tech/tech",
-      link: "https://www.mysql.com/",
-    },
-    spring: {
-      name: "Java Spring",
-      icon: "tech/tech",
-      link: "https://spring.io/", 
-    },
-    markdown: {
-      name: "Markdown",
-      icon: "tech/markdown",
-      link: "https://www.markdownguide.org/",
-    },
-    latex: {
-      name: "LaTeX",
-      icon: "tech/latex",
-      link: "https://www.latex-project.org/",
-    },
-    luatex: {
-      name: "LuaTeX",
-      icon: "tech/lua_colorless",
-      link: "https://www.luatex.org/",
-    },
-    r: {
-      name: "R",
-      icon: "tech/r_colorless",
-      link: "https://www.r-project.org/",
-    },
-    d3: {
-      name: "D3.js",
-      icon: "tech/d3_colorless",
-      link: "https://d3js.org/",
-    },
-    angularjs: {
-      name: "AngularJS",
-      icon: "tech/angularjs_colorless",
-      link: "https://angularjs.org/",
-    },
-    angular2: {
-      name: "Angular 2",
-      icon: "tech/angular2_colorless",
-      link: "https://angular.dev/",
-    },
-    techstack_a: {
-      name: "Tech A",
-      icon: "tech/tech",
-      link: "https://www.google.com/",
-    },
-    techstack_b: {
-      name: "Tech B",
-      icon: "tech/tech",
-      link: "https://www.google.com/",
-    },
-    techstack_c: {
-      name: "Tech C",
-      icon: "tech/tech",
-      link: "https://www.google.com/",
-    },
-  },
+  techstack: techstackDefault
 };
 
 // Check if userGeneralParameters is defined and not empty
--- website/src/data/general.config.ts
+++ website/src/data/general.config.ts
@@ -1,5 +1,5 @@
 import type { GeneralParameters } from "./types/GeneralParameters";
-
+import { techstackDefault } from "./_techstack";
 
 // DEFINE USER PARAMETERS IN HERE;
 // OTHERWISE PLACEHOLDER PARAMETERS WILL BE USED
@@ -17,7 +17,8 @@ export const generalParametersUser: GeneralParameters = {
   city: "Cockayne",
   country: "UK",
   addressLink: "https://maps.app.goo.gl/WVy79Q7bq66XKHYZ8",
-  cvLink: "https://cversatile-demo.netlify.app/profiles/examples/johnDoe/published_pdfs/cv_johnDoe_FEN.pdf",
+  cvLink:
+    "https://cversatile-demo.netlify.app/profiles/examples/johnDoe/published_pdfs/cv_johnDoe_FEN.pdf",
   postcode: "99999",
   birthYear: 1990,
   birthMonth: 1,
@@ -63,125 +64,12 @@ export const generalParametersUser: GeneralParameters = {
     },
   ],
   techstack: {
+    ...techstackDefault,
+    // HERE YOU MAY ADD TO THE DEFAULT TECHSTACK OR OVERWRITE EXISTING TECHSTACK ENTRIES
     astro: {
       name: "Astro",
       icon: "tech/astro",
       link: "https://astro.build/",
-    },
-    javascript: {
-      name: "JavaScript",
-      icon: "tech/typescript",
-      link: "https://www.typescriptlang.org/",
-    },
-    typescript: {
-      name: "TypeScript",
-      icon: "tech/typescript",
-      link: "https://www.typescriptlang.org/",
-    },
-    jquery: {
-      name: "jQuery",
-      icon: "tech/jquery",
-      link: "https://jquery.com/",
-    },
-    bootstrap: {
-      name: "Bootstrap",
-      icon: "tech/bootstrap",
-      link: "https://getbootstrap.com/",
-    },
-    python: {
-      name: "Python",
-      icon: "tech/python",
-      link: "https://www.python.org/",
-    },
-    flask: {
-      name: "Flask",
-      icon: "tech/flask",
-      link: "https://flask.palletsprojects.com/en/stable/",
-    },
-    pyspark: {
-      name: "PySpark",
-      icon: "tech/pyspark2",
-      link: "https://spark.apache.org/docs/latest/api/python/",
-    },
-    psytoolkit: {
-      name: "PsyToolkit",
-      icon: "tech/r_colorless",
-      link: "https://www.psytoolkit.org/experiment-library/",
-    },
-    jsx: {
-      name: "JSX",
-      icon: "tech/jsx",
-      link: "https://fr.legacy.reactjs.org/docs/introducing-jsx.html",
-    },
-    html: {
-      name: "HTML",
-      icon: "tech/html",
-      link: "https://developer.mozilla.org/fr/docs/Web/HTML",
-    },
-    scss: {
-      name: "SCSS",
-      icon: "tech/sass",
-      link: "https://sass-lang.com/",
-    },
-    mysql: {
-      name: "SQL",
-      icon: "tech/tech",
-      link: "https://www.mysql.com/",
-    },
-    spring: {
-      name: "Java Spring",
-      icon: "tech/tech",
-      link: "https://spring.io/", 
-    },
-    markdown: {
-      name: "Markdown",
-      icon: "tech/markdown",
-      link: "https://www.markdownguide.org/",
-    },
-    latex: {
-      name: "LaTeX",
-      icon: "tech/latex",
-      link: "https://www.latex-project.org/",
-    },
-    luatex: {
-      name: "LuaTeX",
-      icon: "tech/lua_colorless",
-      link: "https://www.luatex.org/",
-    },
-    r: {
-      name: "R",
-      icon: "tech/r_colorless",
-      link: "https://www.r-project.org/",
-    },
-    d3: {
-      name: "D3.js",
-      icon: "tech/d3_colorless",
-      link: "https://d3js.org/",
-    },
-    angularjs: {
-      name: "AngularJS",
-      icon: "tech/angularjs_colorless",
-      link: "https://angularjs.org/",
-    },
-    angular2: {
-      name: "Angular 2",
-      icon: "tech/angular2_colorless",
-      link: "https://angular.dev/",
-    },
-    techstack_a: {
-      name: "Tech A",
-      icon: "tech/tech",
-      link: "https://www.google.com/",
-    },
-    techstack_b: {
-      name: "Tech B",
-      icon: "tech/tech",
-      link: "https://www.google.com/",
-    },
-    techstack_c: {
-      name: "Tech C",
-      icon: "tech/tech",
-      link: "https://www.google.com/",
     },
   },
 };
