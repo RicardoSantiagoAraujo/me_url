a63924e9711891dd2e9d935ce58ac936d2203040
ricardoSA54 <ricardosa54@gmail.com>
2025-11-01 20:04:51 +0100

    update astro.config to use i18n

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -10,5 +10,20 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: "https://me_url.netlify.app/",
-  integrations: [preact(), icon(), mdx()]
+  integrations: [preact(), icon(), mdx()],
+  i18n: {
+    locales: ["en", "fr", "pt", "de"],
+    defaultLocale: "en",
+    // When a page in one language doesn’t exist (e.g. a page that is not yet translated), instead of displaying a 404 page, you can choose to display fallback content from another locale on a per-language basis.
+    fallback: {
+      fr: "en",
+      pt: "en",
+      de: "en"
+    },
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true,
+      fallbackType: "rewrite"
+    }
+  }
 });
