c6e5933f447a0d04ff6ea1319a5fbd03ca1e588f
ricardoSA54 <ricardosa54@gmail.com>
2025-11-13 22:30:28 +0100

    use short description in glossary tooltip

--- website/src/data/glossaries/tags.config.ts
+++ website/src/data/glossaries/tags.config.ts
@@ -3,5 +3,4 @@ import type { Tag } from "@/types/Tag";
 // DEFINE USER TAGS IN HERE;
 // HERE YOU MAY ADD TO THE DEFAULT TAGS OR OVERWRITE EXISTING TAGS ENTRIES
 export const tagsUser: Record<string, Tag> = {
-
 };
--- website/src/data/glossaries/techstack.config.ts
+++ website/src/data/glossaries/techstack.config.ts
@@ -3,9 +3,4 @@ import type { Tech } from "@/types/Tech";
 // DEFINE USER TECHSTACK IN HERE;
 // HERE YOU MAY ADD TO THE DEFAULT TECHSTACK OR OVERWRITE EXISTING TECHSTACK ENTRIES
 export const techstackUser: Record<string, Tech> = {
-  astro: {
-    name: "Astro",
-    icon: "tech/astro",
-    link: "https://astro.build/",
-  },
 };
