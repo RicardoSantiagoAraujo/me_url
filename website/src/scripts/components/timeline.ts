40c31bcd40f267dc0c0d875a2bc8d1d46ace7e69
ricardoSA54 <ricardosa54@gmail.com>
2025-11-16 01:08:45 +0100

    vertical timeline completed

--- website/src/scripts/components/timeline.ts
+++ website/src/scripts/components/timeline.ts
@@ -1,39 +1,48 @@
 /**
- * Handles the timeline script
+ * function to animate timeline bars and add balloons in sequence
  * @returns void
  */
 export default function timelineAnimation(): void {
-  var timeline = document.querySelector<HTMLElement>(".timeline");
-  var balloons = document.querySelectorAll<HTMLElement>(".balloon"); // array of all sections with the reveal class (the sections of the html proper that we want to animate)
+  var timelines = document.querySelectorAll<HTMLElement>(".timeline");
   var initial_delay = 2;
   var adjuster = 0.3;
-  var balloon_count = balloons.length;
-  // function to animate timeline bar and add balloons in sequence
-
-  timeline?.classList.add("timeline_reveal");
-  for (var i = 0; i < balloons.length; i++) {
-    balloons[i].classList.add("balloon_reveal");
-    var delay = initial_delay + i * adjuster;
-    balloons[i].style.animationDelay = delay + "s";
-  }
-  setTimeout(function () {
+  // for each timeline in page
+  timelines?.forEach((timeline) => {
+    timeline.classList.add("timeline_reveal");
+    var balloons = timeline.querySelectorAll<HTMLElement>(".balloon"); // array of all sections with the reveal class (the sections of the html proper that we want to animate)
+    var balloon_count = balloons.length;
     for (var i = 0; i < balloons.length; i++) {
-      balloons[i].style.animationDelay = "0s";
-      balloons[i].classList.add("balloon_revealed");
+      balloons[i].classList.add("balloon_reveal");
+      var delay = initial_delay;
+      if (timeline.classList.contains("horizontal")) {
+        delay += i * adjuster;
+      }
+      if (timeline.classList.contains("vertical")) {
+        delay -= i * adjuster;
+      }
+      balloons[i].style.animationDelay = delay + "s";
     }
-    timeline!.style.animation = "none";
-  }, (initial_delay + adjuster * balloon_count) * 1000);
+    setTimeout(function () {
+      for (var i = 0; i < balloons.length; i++) {
+        balloons[i].style.animationDelay = "0s";
+        balloons[i].classList.add("balloon_revealed");
+      }
+      timeline!.style.animation = "none";
+    }, (initial_delay + adjuster * balloon_count) * 1000);
 
-  // to avoid issues on pageback
-  setTimeout(function () {
-    var styleElem = document.head.appendChild(document.createElement("style"));
+    // to avoid issues on pageback
+    setTimeout(function () {
+      var styleElem = document.head.appendChild(
+        document.createElement("style")
+      );
 
-    styleElem.innerHTML = ".balloon_revealed div::before {animation: none;}";
+      styleElem.innerHTML = ".balloon_revealed div::before {animation: none;}";
 
-    for (var i = 0; i < balloons.length; i++) {
-      balloons[i].style.animation = "none";
-    }
-  }, (initial_delay + adjuster * balloon_count) * 1000 + 1000);
+      for (var i = 0; i < balloons.length; i++) {
+        balloons[i].style.animation = "none";
+      }
+    }, (initial_delay + adjuster * balloon_count) * 1000 + 1000);
+  });
 }
 
 setTimeout(() => {
