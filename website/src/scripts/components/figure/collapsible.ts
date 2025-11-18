2ffa80a1e505cb868e54258d3960a5f9415916de
ricardoSA54 <ricardosa54@gmail.com>
2025-11-18 23:43:19 +0100

    adapt collapsible captions to view transitions

--- website/src/scripts/components/figure/collapsible.ts
+++ website/src/scripts/components/figure/collapsible.ts
@@ -1,28 +1,36 @@
+import { listenViewTransitions } from "@/scripts/animation/viewTransitions";
+
 /**
  * Handles collapsible captions functionality.
  * @param collapsibleElement - The collapsible element to activate.
  */
-export function activateCollapsibleCaptions(collapsibleElement: HTMLElement) : void {
-    // if the value is an empty string, remove collapsible
-    if (collapsibleElement.nextElementSibling!.innerHTML.replace(/\s+/g,'') == ""){
-      collapsibleElement.parentElement!.style.display="none";
-    }
-    collapsibleElement.addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling! as HTMLElement; 
-      if (content.style.maxHeight){
-        content.style.maxHeight = "";
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
+export function activateCollapsibleCaptions(
+  collapsibleElement: HTMLElement
+): void {
+  // if the value is an empty string, remove collapsible
+  if (
+    collapsibleElement.nextElementSibling!.innerHTML.replace(/\s+/g, "") == ""
+  ) {
+    collapsibleElement.parentElement!.style.display = "none";
   }
-
-var coll = document.getElementsByClassName("collapsible")! as HTMLCollectionOf<HTMLElement>;
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  activateCollapsibleCaptions(coll[i]);
+  collapsibleElement.addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling! as HTMLElement;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = "";
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
- 
- 
+
+listenViewTransitions(() => {
+  var coll = document.getElementsByClassName(
+    "collapsible"
+  )! as HTMLCollectionOf<HTMLElement>;
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    activateCollapsibleCaptions(coll[i]);
+  }
+}, true);
