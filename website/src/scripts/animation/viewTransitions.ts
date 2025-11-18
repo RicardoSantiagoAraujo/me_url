0220b65585581fe775f63175f1f901bf22eefa4a
ricardoSA54 <ricardosa54@gmail.com>
2025-11-18 21:08:14 +0100

    adapt backToTopButton, DarkModelIcon to view transitions

--- website/src/scripts/animation/viewTransitions.ts
+++ website/src/scripts/animation/viewTransitions.ts
@@ -1,11 +1,12 @@
 
 /**
  * Sets up a listener for Astro view transitions to re-execute a given function.
- * 
+ *
  * @param functionName - The function to be executed on initial load and after Astro view transitions.
  * @param runOnInit - Whether to run the function on initial load.
  */
 export function listenViewTransitions(functionName: () => void, runOnInit: boolean = false) : void {
+  console.log("View transition, listening")
   // Run function on initial load
   if (runOnInit) {
     functionName();
