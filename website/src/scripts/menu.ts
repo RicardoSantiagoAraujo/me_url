16cdfaf03a601107c9307292f248760b44247e93
Ricardo Santiago Araujo <ricardo.santiago-araujo@livingpackets.com>
2025-11-21 14:20:52 +0100

    navigation dropdown: close on outside click

--- website/src/scripts/menu.ts
+++ website/src/scripts/menu.ts
@@ -4,4 +4,11 @@ listenViewTransitions(() => {
   document.querySelector(".hamburger")?.addEventListener("click", () => {
     document.querySelector(".nav-links")?.classList.toggle("expanded");
   });
+  document.querySelector(".hamburger")?.addEventListener("click", () => {
+    document.querySelector(".nav-links__bg-screen")?.classList.toggle("expanded");
+  });
+  document.querySelector(".nav-links__bg-screen")?.addEventListener("click", () => {
+    document.querySelector(".nav-links")?.classList.toggle("expanded");
+    document.querySelector(".nav-links__bg-screen")?.classList.toggle("expanded");
+  });
 }, true);
